ecf6571e3c072b86c9b85ff869b547104bebc536
Ped90 <Ped90@Ped90>
2016-12-11 18:02:46 +0100

    Creamos una nueva clase llamada calculo y metemos el main en ella. Introducimos lo que tenia el main dentro, no todo.

--- src/numprimos/Numprimos.java
+++ src/numprimos/Numprimos.java
@@ -32,13 +32,7 @@ public class NumerosPrimos {
     }
     
     
-        int numDigitos=0;
-        int ndigitos=0;
-        numDigitos=Integer.parseInt(arg[0]);
-        if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
+        
             int divisionEntera = i;
  
             int contador=0;
